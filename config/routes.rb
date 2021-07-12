0ee2d7df07f15303f169eb1e88538fd96f23a533
Ben Quartermaine <ben.quartermaine@ausmed.com.au>
2021-07-13 07:55:48 +1000

    added root, installed git env file to protect secret key

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:create, :new, :destroy]
   end
-  
+
   resources :movies, only: [:index, :show]
 end
